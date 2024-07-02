49a04f366cf68477650e086ce465c28460ae4e46
benfic4rthur <arthur_benfica@hotmail.com>
2024-07-02 14:10:37 -0300

    Instalado o angular material via ng add @angular/material

--- src/app/card-button/card-button.component.ts
+++ src/app/card-button/card-button.component.ts
@@ -4,29 +4,29 @@ import { Component } from '@angular/core';
   selector: 'app-card-button',
   // templateUrl: './card-button.component.html',
   template: `<button class="card-button">Adquirir</button>`, // utilizando template inline
-  // styleUrl: './card-button.component.scss',
-  styles: [ // utilizando styles inline
-    `
-    .card-button {
-    font-size: 16px;
-    text-align: center;
-    margin-top: 10px;
-    border-radius: 25px;
-    background-color: white;
-    cursor: pointer;
-    transition: .7s;
-    padding: 10px;
-    width: 100%;
+  styleUrl: './card-button.component.scss',
+//   styles: [ // utilizando styles inline
+//     `
+//     .card-button {
+//     font-size: 16px;
+//     text-align: center;
+//     margin-top: 10px;
+//     border-radius: 25px;
+//     background-color: white;
+//     cursor: pointer;
+//     transition: .7s;
+//     padding: 10px;
+//     width: 100%;
     
-    &:hover {
-        transition: .7s;
-        background-color: black; // cor de fundo ao passar o mouse
-        color: white;
-        transform: scale(1.03); // aumenta o tamanho ao passar o mouse
-    }
-}
-    `
-  ]
+//     &:hover {
+//         transition: .7s;
+//         background-color: black; // cor de fundo ao passar o mouse
+//         color: white;
+//         transform: scale(1.03); // aumenta o tamanho ao passar o mouse
+//     }
+// }
+//     `
+//   ]
 })
 export class CardButtonComponent {
 
